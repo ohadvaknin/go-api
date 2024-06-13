8ea96e8217e8534ae8d0f2ecd08be5a4af3e14d3
ohadvaknin <ohad.vaknin@post.runi.ac.il>
2024-06-13 21:44:21 +0300

    dockerfile

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/ohadvaknin/go-api/handlers"
 )
+func LoggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Log request method, URI, and timestamp
+        log.Printf("%s %s %s", r.Method, r.RequestURI, time.Now().Format(time.RFC1123))
+
+        // Read and log the request body
+        var bodyBytes []byte
+        if r.Body != nil {
+            bodyBytes, _ = io.ReadAll(r.Body)
+        }
+
+        r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // Restore the request body
+        log.Printf("Request Body: %s", string(bodyBytes))
+
+        // Call the next handler
+        next.ServeHTTP(w, r)
+    })
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -17,6 +38,7 @@ func main() {
 	router.HandleFunc("/tasks/{id}", handlers.GetTask).Methods("GET")
 	router.HandleFunc("/tasks/{id}", handlers.UpdateTask).Methods("PUT")
 	router.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods("DELETE")
+	loggedRouter := LoggingMiddleware(router)
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", loggedRouter))
 }	
